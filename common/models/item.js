b59ac44375ddcabf69737edb44ae1cda4f8f3b75
JensDeCraecker <jens.decraecker@student.ap.be>
2018-06-05 13:06:20 +0200

    Add all coords

--- common/models/item.js
+++ common/models/item.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = (Item) => {
-
+  Item.remoteMethod(
+    'items',
+    {
+      http: { verb: 'delete' },
+      returns: { arg: 'items', type: 'Array' },
+    });
 };
